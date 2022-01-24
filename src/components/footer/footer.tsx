dfd85981681feea263d6ff79d8112f8202fbdaf9
George <gfilippou@gmail.com>
2022-01-24 13:19:09 +0200

    updated info

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,9 +5,7 @@ export function Footer() {
     <div className={styles.footer}>
       Created by George Filippou
       <a href='https://github.com/SimpleFi-finance/frontend-test'>Specs</a>
-      <a href='https://github.com/SimpleFi-finance/frontend-test'>
-        Github repo
-      </a>
+      <a href='https://github.com/gfilippou/graph'>Github repo</a>
     </div>
   );
 }
